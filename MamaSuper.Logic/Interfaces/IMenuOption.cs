4b11abc819c90c62ec73fade5d5a3fd5dafdbfc9
Iftah Roichman <ifttah@gmail.com>
2020-06-10 22:41:06 +0300

    Menu option logic

---
+++ MamaSuper.Logic/Interfaces/IMenuOption.cs
@@ -0,0 +1,15 @@
+﻿namespace MamaSuper.Logic.Interfaces
+{
+    public interface IMenuOption
+    {
+        /// <summary>
+        /// The option description for the choice menu 
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// The action the action preforms
+        /// </summary>
+        void Action();
+    }
+}
