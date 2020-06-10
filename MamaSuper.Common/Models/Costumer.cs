bd5aa6b789506a96e75218eab9cc0e3b8d0429df
Iftah Roichman <ifttah@gmail.com>
2020-06-10 22:43:14 +0300

    Line management part

---
+++ MamaSuper.Common/Models/Costumer.cs
@@ -0,0 +1,10 @@
+﻿namespace MamaSuper.Common.Models
+{
+    public class Costumer : Person
+    {
+        public Costumer(string name, int bodyTemperature, bool hasMask, bool shouldIsolate) : base(name,
+            bodyTemperature, hasMask, shouldIsolate)
+        {
+        }
+    }
+}
---
+++ MamaSuper.Common/Utils/ConsoleUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MamaSuper.Common.Utils
+{
+    /// <summary>
+    /// Console utils
+    /// </summary>
+    public static class ConsoleUtils
+    {
+        /// <summary>
+        /// Gets user input based on a specific output
+        /// </summary>
+        public static string GetInputByOutput(string output)
+        {
+            Console.WriteLine(output);
+            return Console.ReadLine();
+        }
+    }
+}
