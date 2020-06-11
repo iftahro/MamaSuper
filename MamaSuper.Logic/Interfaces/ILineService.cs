1ee266c06480d3c07da7ed8712f09fe819e9df83
Iftah Roichman <ifttah@gmail.com>
2020-06-11 20:31:46 +0300

    Fixed doc

--- MamaSuper.Logic/Interfaces/ILineService.cs
+++ MamaSuper.Logic/Interfaces/ILineService.cs
@@ -2,10 +2,30 @@
 
 namespace MamaSuper.Logic.Interfaces
 {
+    /// <summary>
+    /// Generic line service for comfortable line management
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface ILineService<T>
     {
+        /// <summary>
+        /// Tries adding an item to the line
+        /// </summary>
+        /// <param name="item">The item to be added</param>
+        /// <param name="failingMessage">The outcome message (failed/succeeded)</param>
+        /// <returns>Is the adding was successful</returns>
         bool TryAddItemToLine(T item, out string failingMessage);
+
+        /// <summary>
+        /// Gets all line items
+        /// </summary>
         IList<T> GetLineItems();
+
+        /// <summary>
+        /// Moves out items from line
+        /// </summary>
+        /// <param name="count">The number of items to move out</param>
+        /// <returns>The items that where moved out</returns>
         IEnumerable<T> MoveItemsFromLine(int count);
     }
 }
